41a0e16ebcaf439475f88987fc73421eabf7aad6
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-03-21 11:21:17 +0000

    feat: display updatebot version before attempting to update

--- build.sh
+++ build.sh
@@ -49,6 +49,7 @@ fi
 retry 3 skaffold build -f skaffold.yaml
 
 if [ "release" == "${RELEASE}" ]; then
+  updatebot version
   # trigger downstream release
   updatebot push-version --kind docker \
     jenkinsxio/builder-baseimage ${VERSION} \
