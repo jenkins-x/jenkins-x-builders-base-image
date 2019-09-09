e3f18400b41ff06cc4056eb29bc30dff9079555b
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-09-09 10:29:32 +0100

    fix: downstream repo build

--- update-bot.sh
+++ update-bot.sh
@@ -7,14 +7,14 @@ set -o pipefail
 jx step create pr docker \
     --name gcr.io/jenkinsxio/builder-baseimage \
     --version ${VERSION} \
-    --repo https://github.com/jenkins-x/jenkins-x-builder-base.git
+    --repo https://github.com/jenkins-x/jenkins-x-builders-base.git
 
 jx step create pr docker \
     --name gcr.io/jenkinsxio/builder-ruby-baseimage \
     --version ${VERSION} \
-    --repo https://github.com/jenkins-x/jenkins-x-builder-base.git
+    --repo https://github.com/jenkins-x/jenkins-x-builders-base.git
 
 jx step create pr docker \
     --name gcr.io/jenkinsxio/builder-swift-baseimage \
     --version ${VERSION} \
-    --repo https://github.com/jenkins-x/jenkins-x-builder-base.git
+    --repo https://github.com/jenkins-x/jenkins-x-builders-base.git
